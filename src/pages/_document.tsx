b964ef21912bb7e2e45e792570be9dd4aceda652
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-08-06 20:56:13 +0700

    chore: update document and index

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,12 +16,18 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html data-theme="forest">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap"
+            rel="stylesheet"
+          />
+          {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+          <title>Title</title>
         </Head>
         <body>
           <Main />
