b1b3f79cec5d301e3e505594f3e7d375a6007c66
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-08-06 23:58:27 +0700

    refactor: use collapse library instead

--- src/components/Dalil.tsx
+++ src/components/Dalil.tsx
@@ -2,15 +2,32 @@ import React from "react"
 
 import { FaStarAndCrescent } from "react-icons/fa"
 
+import CollapseItem from "@components/CollapseItem"
+import { DALILS } from "data/dalil"
+
 const Dalil = () => {
   return (
     <div>
-      <div className="flex items-center justify-center text-center">
+      <div className="mb-2 flex items-center justify-center text-center">
         <FaStarAndCrescent />
-        <h3 className="ml-3 text-lg font-bold">Dalil-Dalil Dalam Pernikahan</h3>
+        <h3 className="ml-3 py-3 text-lg font-bold">
+          Dalil-Dalil Dalam Pernikahan
+        </h3>
       </div>
-      <div className="py-5 italic">Coming Soon</div>
-      {/*<Collapse title="QS Ar-Rum 21" content="Ar-Rum" />*/}
+      <div className="dalil h-96 w-full items-center justify-center overflow-y-auto py-3 text-center">
+        {DALILS.map(({ title, arab, intro, content, source }) => {
+          return (
+            <CollapseItem
+              key={title}
+              title={title}
+              arab={arab}
+              intro={intro}
+              content={content}
+              source={source}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
