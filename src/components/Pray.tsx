ba619d256a0a8a98aa01d5ee12affa8ef8dde530
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-08-07 03:21:45 +0700

    feat: add Pray.tsx

---
+++ src/components/Pray.tsx
@@ -0,0 +1,18 @@
+import Card from "@components/Card"
+
+const Pray = () => {
+  return (
+    <Card title="Doa Untuk Pengantin">
+      <div className="my-3 font-serif text-3xl">
+        بَارَكَ اللهُ لَكَ وَبَارَكَ عَلَيْكَ وَجَمَعَ بَيْنَكُمَا فِي خَيْرٍ
+      </div>
+      <div>
+        “Semoga Allah memberkahimu di waktu bahagia dan memberkahimu di waktu
+        susah, serta semoga Allah mempersatukan kalian berdua dalam kebaikan”
+      </div>
+      <div>(HR. Abu Dawud no. 2130)</div>
+    </Card>
+  )
+}
+
+export default Pray
