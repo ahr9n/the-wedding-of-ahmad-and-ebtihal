da2d7fc633f6cadc74cb75656985779cee738b24
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-08-06 23:57:31 +0700

    feat: add dalil-dalil

---
+++ data/dalil.ts
@@ -0,0 +1,62 @@
+import { IDalil } from "./interfaces"
+
+export const DALILS: IDalil[] = [
+  {
+    title: "QS Ar-Rum Ayat 21",
+    arab: "وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُمْ مِنْ أَنْفُسِكُمْ أَزْوَاجًا لِتَسْكُنُوا إِلَيْهَا وَجَعَلَ بَيْنَكُمْ مَوَدَّةً وَرَحْمَةً ۚ إِنَّ فِي ذَٰلِكَ لَآيَاتٍ لِقَوْمٍ يَتَفَكَّرُونَ",
+    intro: null,
+    source: "QS Ar-Rum (30): 21",
+    content:
+      "Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan untukmu isteri-isteri dari jenismu sendiri, supaya kamu cenderung dan merasa tenteram kepadanya, dan dijadikan-Nya diantaramu rasa kasih dan sayang. Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda bagi kaum yang berfikir",
+  },
+  {
+    title: "QS Az-Zariyat Ayat 49",
+    arab: "وَمِنْ كُلِّ شَيْءٍ خَلَقْنَا زَوْجَيْنِ لَعَلَّكُمْ تَذَكَّرُونَ",
+    intro: null,
+    source: "QS Az-Zariyat (51): 49",
+    content:
+      "Dan segala sesuatu Kami Ciptakan Berpasang-pasangan supaya kamu mengingat kebesaran Allah",
+  },
+  {
+    title: "QS Al-Hujurat Ayat 13",
+    arab: "يَٰٓأَيُّهَا ٱلنَّاسُ إِنَّا خَلَقۡنَٰكُم مِّن ذَكَرٖ وَأُنثَىٰ وَجَعَلۡنَٰكُمۡ شُعُوبٗا وَقَبَآئِلَ لِتَعَارَفُوٓاْۚ إِنَّ أَكۡرَمَكُمۡ عِندَ ٱللَّهِ أَتۡقَىٰكُمۡۚ إِنَّ ٱللَّهَ عَلِيمٌ خَبِيرَٞ",
+    intro: null,
+    source: "QS Al-Hujurat (49): 13",
+    content:
+      "Hai manusia, sesungguhnya Kami menciptakan kamu dari seorang laki-laki dan seorang perempuan dan menjadikan kamu berbangsa-bangsa dan bersuku-suku supaya kamu saling kenal-mengenal. Sesungguhnya orang yang paling mulia diantara kamu disisi Allah ialah orang yang paling takwa di antara kamu. Sesungguhnya Allah Maha Mengetahui lagi Maha Mengenal",
+  },
+  {
+    title: "Menyempurnakan 1/2 Agama",
+    arab: "إِذَا تَزَوَّجَ العَبْدُ فَقَدْ كَمَّلَ نَصْفَ الدِّيْنِ ، فَلْيَتَّقِ اللهَ فِي النِّصْفِ البَاقِي",
+    intro:
+      "Dari Anas bin Malik radhiyallahu ‘anhu,  ia berkata bahwa Rasulullah shallallahu ‘alaihi wa sallam bersabda",
+    source: "(HR. Al Baihaqi)",
+    content:
+      "Jika seseorang menikah, maka ia telah menyempurnakan separuh agamanya. Karenanya, bertakwalah pada Allah pada separuh yang lainnya",
+  },
+  {
+    title: "Dapat Menjaga Dari Yang Haram",
+    arab: "إيَا مَعْشَرَ الشَّبَابِ مَنِ اسْتَطَاعَ مِنْكُمُ الْبَاءَةَ فَلْيَتَزَوَّجْ فَإِنَّهُ أَغَضُّ لِلْبَصَرِ وَأَحْصَنُ لِلْفَرْجِ وَمَنْ لَمْ يَسْتَطِعْ فَعَلَيْهِ بِالصَّوْمِ فَإِنَّهُ لَهُ وِجَاءٌ",
+    intro: "Rasulullah shallallahu ‘alaihi wa sallam bersabda",
+    source: "(HR. Bukhari no. 5065 dan HR. Muslim no. 1400)",
+    content:
+      "Hai sekalian pemuda, barangsiapa di antara kalian sudah memiliki kemampuan, segeralah menikah, karena menikah dapat menundukkan pandangan dan memelihara kemaluan. Dan barangsiapa yang belum sanggup menikah, berpuasalah, karena puasa akan menjadi benteng baginya",
+  },
+  {
+    title: "3 Golongan Yang Ditolong Allah ﷻ",
+    arab: "ثَلاَثَةٌ حَقٌّ عَلَى اللهِ عَوْنُهُمْ الْمُجَاهِدُ فِي سَبِيلِ اللهِ وَالمُكَاتَبُ الَّذِي يُرِيدُ الأَدَاءَ وَالنَّاكِحُ الَّذِي يُرِيدُ العَفَافَ",
+    intro: "Rasulullah shallallahu ‘alaihi wa sallam bersabda",
+    source:
+      "(HR. At-Tirmidzi dari Abu Hurairah radhiyallahu’anhu, Shahihul Jami’: 3050)",
+    content:
+      "Ada tiga orang yang Allah wajibkan atas diri-Nya untuk menolong mereka, Orang yang berjihad di jalan Allah, Budak yang memiliki perjanjian yang berniat memenuhi perjanjiannya, dan orang yang menikah dengan niat menjaga kesucian diri dari perzinahan",
+  },
+  {
+    title: "Memperbanyak Umat Rasulullah ﷺ",
+    arab: "إتزوجوا فإني مكاثر بكم يوم القيامة",
+    intro: "Rasulullah shallallahu ‘alaihi wa sallam bersabda",
+    source: "(HR. Abu Daud)",
+    content:
+      "Menikahlah kalian. Karena aku begitu bangga dengan banyaknya umatku pada hari kiamat",
+  },
+]
