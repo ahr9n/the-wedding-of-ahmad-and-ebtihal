9a7d012a36045972fe732932b3387b262157f32a
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-08-08 04:37:30 +0700

    feat: add draft footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer footer-center bg-neutral p-5">
+        <div>
+          <p className="font-bold">Created by Muhammad Hasan</p>
+          <p>Copyright © 2022 - All right reserved</p>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
