95cffd9fc5dd62ed9f54fd2384db0c1ea11dce38
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-09-12 14:19:26 +0200

    Update ThankYou.tsx

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -3,7 +3,7 @@ import { renderCoupleNames } from "@utils/utils"
 
 const ThankYou = () => {
   return (
-    <Card title="شكرا لكم">
+    <Card title="شكرًا لكم">
       <div>تشرّفنا بحضوركم، والعاقبة عندكم في المسرّات</div>
       {renderCoupleNames()}
     </Card>
