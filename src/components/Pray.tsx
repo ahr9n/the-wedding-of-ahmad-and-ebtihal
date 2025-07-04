29a1ef728782444252120365f5ead1d47ab3739c
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2025-07-04 15:46:05 +0300

    remove extra text

--- src/components/Pray.tsx
+++ src/components/Pray.tsx
@@ -8,12 +8,6 @@ const Pray = () => {
       <div className="my-3 font-ruqaa text-3xl">
         May Allah bless you, and shower His blessings upon you, and bring you together in goodness.
       </div>
-      <div>
-        May Allah grant you blessings and goodness in your marriage, 
-        may goodness descend upon you, 
-        and may He unite you in obedience, health, and all that is good.
-      </div>
-      <div>Authentic - Abu Dawood, Hadith No. 2130</div>
       {renderSendPrayButton()}
     </Card>
   )
