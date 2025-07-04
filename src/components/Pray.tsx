44eb72a362115ef2c433634c89aa769b5ee348e6
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2025-07-04 15:32:41 +0300

    disable prettier

--- src/components/Pray.tsx
+++ src/components/Pray.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import Card from "@components/Card"
 import { renderSendPrayButton } from "@utils/utils"
 
--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import Card from "@components/Card"
 import { renderCoupleNames } from "@utils/utils"
 
